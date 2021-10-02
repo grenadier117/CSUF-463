82ecedd62d4e2926e4f2487062945afb89c1daae
Jake Hamo <jake.hamo@neudesic.com>
2021-10-02 14:18:20 -0700

    file rename

--- src/types/index.ts
+++
@@ -1,3 +0,0 @@
-import { RootState } from './rootState';
-
-export type { RootState };
--- src/types/rootState.ts
+++
@@ -1,5 +0,0 @@
-import { IGlobal } from "app/global/global.redux";
-
-export interface RootState {
-  global?: IGlobal;
-}
